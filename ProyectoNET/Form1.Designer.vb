428f85996638b2cf94afc58f027873aee95d1b61
DAETHERION\Ricar <ricardogomezgamito@gmail.com>
2022-02-20 02:08:59 +0100

    En mitad de función de busqueda de programas.

---
+++ ProyectoNET/Form1.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class SuiteApp
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Correo = New System.Windows.Forms.Button()
+        Me.Periodico = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Correo
+        '
+        Me.Correo.Location = New System.Drawing.Point(76, 52)
+        Me.Correo.Name = "Correo"
+        Me.Correo.Size = New System.Drawing.Size(75, 23)
+        Me.Correo.TabIndex = 0
+        Me.Correo.Text = "Correo"
+        Me.Correo.UseVisualStyleBackColor = True
+        '
+        'Periodico
+        '
+        Me.Periodico.Location = New System.Drawing.Point(212, 52)
+        Me.Periodico.Name = "Periodico"
+        Me.Periodico.Size = New System.Drawing.Size(75, 23)
+        Me.Periodico.TabIndex = 1
+        Me.Periodico.Text = "Periodico"
+        Me.Periodico.UseVisualStyleBackColor = True
+        '
+        'SuiteApp
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(559, 436)
+        Me.Controls.Add(Me.Periodico)
+        Me.Controls.Add(Me.Correo)
+        Me.Name = "SuiteApp"
+        Me.Text = "SuiteApp"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Correo As Button
+    Friend WithEvents Periodico As Button
+End Class
---
+++ ProyectoNET/Form1.vb
@@ -0,0 +1,41 @@
+﻿Public Class SuiteApp
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        BusquedaProgramas()
+    End Sub
+    Private Sub Correo_Click(sender As Object, e As EventArgs) Handles Correo.Click
+        IniciarPrograma("")
+
+    End Sub
+
+    Private Sub Periodico_Click(sender As Object, e As EventArgs) Handles Periodico.Click
+        IniciarPrograma("C:\Windows\system32\notepad.exe")
+    End Sub
+
+    Private Sub IniciarPrograma(arg As String)
+        'Función generica para abrir programas segun el boton que se pulse
+        Dim inicio As New ProcessStartInfo
+        inicio.FileName = arg
+        Process.Start(arg)
+    End Sub
+    Private Sub BusquedaProgramas()
+        'Función para que se busquen las direcciones de los programas que se tienen instalados
+        Dim listaDirectorios As IReadOnlyCollection(Of String)
+        Dim listaFicheros As New List(Of String)
+        Dim listaExe As New List(Of String)
+        listaDirectorios = (My.Computer.FileSystem.GetDirectories("C:\"))
+        For Each directorio As String In listaDirectorios
+            listaFicheros.Add(directorio)
+        Next
+        For Each fichero As String In listaFicheros
+            If StrComp(fichero.Substring(fichero.Count - 4), ".exe") = 0 Then
+                Console.Write("El fichero " + fichero + " es un .exe")
+                listaExe.Add(fichero)
+            Else
+                Console.Write("El fichero " + fichero + " NO es un .exe")
+            End If
+        Next
+
+
+    End Sub
+End Class
+
